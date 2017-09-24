76679b4bdba01eea3f37c1cbb56c52a581d925a3
18640130841 <15734063646@163.com>
2017-09-24 11:17:42 +0800

    feat(): 在windows下无需再去修改Native这个类
    直接将修改完的类编译成class文件，放到resource目录下 启动时复制到classpath即可

--- src/main/java/com/dongweima/data/unit/test/db/config/HbaseConfig.java
+++ src/main/java/com/dongweima/data/unit/test/db/config/HbaseConfig.java
@@ -3,7 +3,6 @@ package com.dongweima.data.unit.test.db.config;
 import com.dongweima.data.unit.test.db.init.DataInitScript;
 import com.dongweima.utils.file.FileUtil;
 import com.dongweima.utils.file.PathUtil;
-import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.slf4j.Logger;
@@ -26,6 +25,11 @@ public class HbaseConfig {
   private static int count = 0;
   private static final HBaseTestingUtility testUtil = new HBaseTestingUtility();
 
+  static {
+    //复制文件
+    copy();
+  }
+
   public static void start() {
     if (count == 0) {
       synchronized (HbaseConfig.class) {
@@ -34,8 +38,7 @@ public class HbaseConfig {
             //todo 考虑将这个设置变量改造成在window下才设置 这个变量可配置
             //考虑引入进来 并且包含 linux和window都可以用的
             //String path = new FileReader(HbaseConfig.class.getResourceAsStream("/")).getPath();
-            //复制文件
-            copy();
+
             DataInitScript.initAll(false);
             System.setProperty("hadoop.home.dir", PathUtil.getBaseDir());
             count++;
@@ -48,22 +51,48 @@ public class HbaseConfig {
     }
   }
 
-  private static void copy() throws IOException {
+  private static void copy() {
+
     copyFileInJarToClasspath("/bin/hadoop1", "bin/hadoop");
     copyFileInJarToClasspath("/bin/hadoop1.cmd", "bin/hadoop.cmd");
     copyFileInJarToClasspath("/bin/hadoop1.dll", "bin/hadoop.dll");
     copyFileInJarToClasspath("/bin/hadoop1.exp", "bin/hadoop.exp");
-    copyFileInJarToClasspath("/bin/hadoop1.lib", "bin/hadoop.lib");
+    //copy failed
+    //copyFileInJarToClasspath("/bin/hadoop1.lib", "bin/hadoop.lib");
     copyFileInJarToClasspath("/bin/hadoop1.pdb", "bin/hadoop.pdb");
     copyFileInJarToClasspath("/bin/rcc1", "bin/rcc.exe");
     copyFileInJarToClasspath("/bin/winutils1.pdb", "bin/winutils.pdb");
     copyFileInJarToClasspath("/bin/winutils1.exe", "bin/winutils.exe");
+    String base = "org/apache/hadoop/io/nativeio/";
+    copyFileInJarToClasspath("/" + base + "NativeIO$CachedUid1.class",
+        base + "NativeIO$CachedUid.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX$CachedName1.class",
+        base + "NativeIO$POSIX$CachedName.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX$CacheManipulator1.class",
+        base + "NativeIO$POSIX$CacheManipulator.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX$IdCache1.class",
+        base + "NativeIO$POSIX$IdCache.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX$NoMlockCacheManipulator1.class",
+        base + "NativeIO$POSIX$NoMlockCacheManipulator.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX$Stat1.class",
+        base + "NativeIO$POSIX$Stat.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$POSIX1.class", base + "NativeIO$POSIX.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$Windows$AccessRight1.class",
+        base + "NativeIO$Windows$AccessRight.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO$Windows1.class",
+        base + "NativeIO$Windows.class");
+    copyFileInJarToClasspath("/" + base + "NativeIO1.class", base + "NativeIO.class");
+
   }
 
   //复制 从jar中的bin目录复制到  项目的resource目录下
   @SuppressWarnings("all")
-  private static void copyFileInJarToClasspath(String source, String dest) throws IOException {
-    FileUtil.copyFileInJarToClasspath(source, dest);
+  private static void copyFileInJarToClasspath(String source, String dest) {
+    try {
+      FileUtil.copyFileInJarToClasspath(source, dest);
+    } catch (Exception e) {
+      logger.error("copy file failed", e);
+    }
   }
 
   public static HBaseTestingUtility getTestUtil() {
