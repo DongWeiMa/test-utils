62eed050007e8072140342d5a50b90f18d28190b
18640130841 <15734063646@163.com>
2017-09-25 10:41:09 +0800

    feat(): 加入四种测试类型 之后每个测试方法上加上

---
+++ src/main/java/com/dongweima/data/unit/test/db/constants/TestType.java
@@ -0,0 +1,15 @@
+package com.dongweima.data.unit.test.db.constants;
+
+public class TestType {
+
+  //测试代码能否正确运行
+  public static final String DESIGN_TEST = "design testing";
+  //测试可能失败的边界条件
+  public static final String BOUND_TEST = "boundary testing";
+  //测试已经失败的bug
+  public static final String BUG_TEST = "bug testing";
+  //强制错误条件塞入测试
+  public static final String ERROR_TEST = "error testing";
+  //性能测试
+  public static final String r = "performance testing";
+}
